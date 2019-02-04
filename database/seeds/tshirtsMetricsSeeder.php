18968682bdcb086ea835efc87e70e74eefcf9202
Jeroense <jeroense@hotmail.com>
2019-02-04 08:18:59 +0100

    Update metrics table

--- database/seeds/tshirtsMetricsSeeder.php
+++ database/seeds/tshirtsMetricsSeeder.php
@@ -11,39 +11,39 @@ class tshirtMetricsSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('TshirtMetrics')->insert([
+        DB::table('tshirtmetrics')->insert([
             'size' => 'XS',
             'length_mm' => 687
         ]);
-        DB::table('TshirtMetrics')->insert([
+        DB::table('tshirtmetrics')->insert([
             'size' => 'S',
             'length_mm' => 711
         ]);
-        DB::table('TshirtMetrics')->insert([
+        DB::table('tshirtmetrics')->insert([
             'size' => 'M',
             'length_mm' => 737
         ]);
-        DB::table('TshirtMetrics')->insert([
+        DB::table('tshirtmetrics')->insert([
             'size' => 'L',
             'length_mm' => 762
         ]);
-        DB::table('TshirtMetrics')->insert([
+        DB::table('tshirtmetrics')->insert([
             'size' => 'XL',
             'length_mm' => 787
         ]);
-        DB::table('TshirtMetrics')->insert([
+        DB::table('tshirtmetrics')->insert([
             'size' => 'XXL',
             'length_mm' => 813
         ]);
-        DB::table('TshirtMetrics')->insert([
+        DB::table('tshirtmetrics')->insert([
             'size' => '3XL',
             'length_mm' => 838
         ]);
-        DB::table('TshirtMetrics')->insert([
+        DB::table('tshirtmetrics')->insert([
             'size' => '4XL',
             'length_mm' => 868
         ]);
-        DB::table('TshirtMetrics')->insert([
+        DB::table('tshirtmetrics')->insert([
             'size' => '5XL',
             'length_mm' => 899
         ]);
